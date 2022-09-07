00d8c94147982e918d36ab2363afa6059244ce50
Farzin Mirzaie <farzin@firstsource.tech>
2022-09-07 10:11:23 +0430

    feat: :sparkles: add `react-navigation`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {StatusBar, useColorScheme} from 'react-native';
+import {StatusBar, TouchableOpacity, useColorScheme} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
 import {Screen, TextPrimary, TextSecondary} from './components';
 import {darkTheme, lightTheme} from './themes';
+
+const Stack = createNativeStackNavigator();
+
+const TestScreen = ({navigation}) => {
+  return (
+    <Screen>
+      <TextPrimary>Primary</TextPrimary>
+      <TextSecondary>Secondary</TextSecondary>
+      <TouchableOpacity onPress={() => navigation.navigate('b')}>
+        <TextSecondary>Test</TextSecondary>
+      </TouchableOpacity>
+    </Screen>
+  );
+};
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -10,10 +26,12 @@ const App = () => {
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Screen>
-        <TextPrimary>Primary</TextPrimary>
-        <TextSecondary>Secondary</TextSecondary>
-      </Screen>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="a" component={TestScreen} />
+          <Stack.Screen name="b" component={TestScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ThemeProvider>
   );
 };
