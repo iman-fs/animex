886ff4e1482954d0ddba0766629f65a008412e8d
Farzin Mirzaie <farzin@firstsource.tech>
2022-09-08 22:31:38 +0430

    feat: :sparkles: add search functionality

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, {useTheme} from 'styled-components/native';
+import styled from 'styled-components/native';
 import {TAnime} from '../types';
 import TextPrimary from './TextPrimary';
 import TextSecondary from './TextSecondary';
@@ -17,6 +17,7 @@ const DetailsContainer = styled.View`
 const Cover = styled.Image`
   width: 75px;
   height: 115px;
+  background-color: ${({theme}) => theme.colors.textSecondary};
 `;
 
 type TProps = {
@@ -24,23 +25,16 @@ type TProps = {
   onPress: () => void;
 };
 
-const AnimeCard: React.FC<TProps> = ({data, onPress}) => {
-  const {colors} = useTheme();
-
-  return (
-    <Container onPress={onPress}>
-      <Cover
-        source={{uri: data.images.jpg.image_url}}
-        style={{backgroundColor: colors.secondary}}
-      />
-      <DetailsContainer>
-        <TextPrimary>{data.title}</TextPrimary>
-        {data.rating && <TextSecondary>{data.rating}</TextSecondary>}
-        {data.score && <TextSecondary>Score: {data.score}</TextSecondary>}
-        {data.year && <TextSecondary>Year: {data.year}</TextSecondary>}
-      </DetailsContainer>
-    </Container>
-  );
-};
+const AnimeCard: React.FC<TProps> = ({data, onPress}) => (
+  <Container onPress={onPress}>
+    <Cover source={{uri: data.images.jpg.image_url}} />
+    <DetailsContainer>
+      <TextPrimary>{data.title}</TextPrimary>
+      {data.rating && <TextSecondary>{data.rating}</TextSecondary>}
+      {data.score && <TextSecondary>Score: {data.score}</TextSecondary>}
+      {data.year && <TextSecondary>Year: {data.year}</TextSecondary>}
+    </DetailsContainer>
+  </Container>
+);
 
 export default AnimeCard;
